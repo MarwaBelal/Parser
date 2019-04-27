c201ef57c8b714e9f062b4b07c1bea9f18fd5682
Marwa Belal <marwabelal105@gmail.com>
2019-04-27 14:03:48 +0200

    update node

--- src/parser/Node.java
+++ src/parser/Node.java
@@ -4,3 +4,26 @@ public interface Node {
 	public void printNode();
 
 }
+/*
+public class Node {
+    int value;
+    Node left;
+    Node right;
+    
+    Node(int value){
+        this.value = value;
+        right = null;
+        left = null;
+        
+    }
+    
+    public void print(Node n){
+        if(n != null){
+            print(n.left);
+            System.out.println("" + n.value);
+            print(n.right);
+        }
+    }
+    
+}*/
+
